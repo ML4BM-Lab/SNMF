ee8a1d35d00de8e43abbc621ffcaf8420e201330
Luis Alonso Esteban <laesteban@unav.es>
2026-03-17 10:56:13 +0100

    Add dataset example

---
+++ R/data.R
@@ -0,0 +1,7 @@
+#' TNBC small dataset. Simulated dataset has been filtered so that 100th most highly variable genes are kept.
+#'
+#' A small dataset for demonstrating package functionality.
+#'
+#' @format A data frame with 100 rows and 2500 columns:
+#' @source Simulated data from Starfysh paper
+"tnbc"
